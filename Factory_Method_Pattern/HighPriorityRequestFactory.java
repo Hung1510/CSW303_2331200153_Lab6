81fefc1020ecc6bf9361f7076f4f55eaa14bbbff
Phạm Trần Gia Hưng <giahung1510200519@gmail.com>
2025-06-03 08:20:50 +0700

    factory update

--- Factory_Method_Pattern/HighPriorityRequestFactory.java
+++
@@ -1,7 +0,0 @@
-package CSW303_2331200153_Lab6.Factory_Method_Pattern;
-public class HighPriorityRequestFactory extends MaintenanceRequestFactory {
-    @Override
-    public MaintenanceRequest createRequest(String description) {
-        return new HighPriorityRequest(description);
-    }
-}
--- Factory_Method_Pattern/MaintenanceRequestFactory.java
+++
@@ -1,4 +0,0 @@
-package CSW303_2331200153_Lab6.Factory_Method_Pattern;
-public abstract class MaintenanceRequestFactory {
-    public abstract MaintenanceRequest createRequest(String description);
-}
